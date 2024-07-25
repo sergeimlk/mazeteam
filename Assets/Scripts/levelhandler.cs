c445d051c9e4b24632df479f951aec514df7807d
jonathan <jonathangager75@gmail.com>
2024-07-25 16:19:45 +0200

    Create menu

---
+++ Assets/Scripts/levelhandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelhandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void level1()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+
+}
