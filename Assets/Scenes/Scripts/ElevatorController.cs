3173fb1cc91becb57db8af49b6c85da4d923f177
segeimlk <sergei.mlk@hotmail.com>
2024-07-25 14:49:12 +0200

    ça roule :)

---
+++ Assets/Scenes/Scripts/ElevatorController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class ElevatorController : MonoBehaviour {
+
+	public float speed;
+	public float upper;
+	public float lower;
+	public Axis axis;
+	public MoveState moveState;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (axis.Equals(Axis.X)) {
+			if (transform.position.x >= upper) {
+				moveState = MoveState.ToLower;
+			} else if (transform.position.x <= lower) {
+				moveState = MoveState.ToUpper;
+			}
+		} else if (axis.Equals(Axis.Y)) {
+			if (transform.position.y >= upper) {
+				moveState = MoveState.ToLower;
+			} else if (transform.position.y <= lower) {
+				moveState = MoveState.ToUpper;
+			}
+		} else if (axis.Equals(Axis.Z)) {
+			if (transform.position.z >= upper) {
+				moveState = MoveState.ToLower;
+			} else if (transform.position.z <= lower) {
+				moveState = MoveState.ToUpper;
+			}
+		}
+			
+		checkMove();
+	}
+
+	private void checkMove() {
+		if (axis.Equals(Axis.X)) {
+			if (moveState.Equals(MoveState.ToUpper)) {
+				transform.Translate(Vector3.right * speed);
+			} else {
+				transform.Translate(Vector3.left * speed);
+			}
+		} else if (axis.Equals(Axis.Y)) {
+			if (moveState.Equals(MoveState.ToUpper)) {
+				transform.Translate(Vector3.up * speed);
+			} else {
+				transform.Translate(Vector3.down * speed);
+			}
+		} else if (axis.Equals(Axis.Z)) {
+			if (moveState.Equals(MoveState.ToUpper)) {
+				transform.Translate(Vector3.forward * speed);
+			} else {
+				transform.Translate(Vector3.back * speed);
+			}
+		}
+	}
+}
+
+public enum Axis {
+	X, Y, Z
+}
+public enum MoveState {
+	ToUpper, ToLower
+}
---
+++ Assets/Scenes/Scripts/Respawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	public float outOfBoundsY = -20;
+	public Vector3 respawnLocation;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (transform.position.y <= outOfBoundsY) {
+			transform.position = respawnLocation;
+		}
+	}
+}
